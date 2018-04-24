3eca1088d4f07ad8fcb6fa56057b564eac7fe927
DeckedSpring67 <deckedspring@gmail.com>
2018-04-24 16:55:46 +0200

    Added form parsing for distro, checks if it's already been created

--- server.go
+++ server.go
@@ -11,7 +11,13 @@ import (
 type distro struct {
 	name    string
 	created bool
-	date    string
+}
+
+type options struct {
+	Browser string
+	Email   string
+	Media   string
+	Office  string
 }
 
 func startBuild(distros []distro) {
@@ -22,25 +28,59 @@ func startBuild(distros []distro) {
 	}
 }
 
+func getSchedule(w http.ResponseWriter, r *http.Request) {
+	fmt.Printf("%s</table>", response)
+}
+
 func createDistro(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()       //Parse url parameters passed, then parse the response packet for the POST body (request body)
 	fmt.Println(r.Form) // print information on server side.
-	name := fmt.Sprintf("arch-%s", time.Date())
-	fmt.Println("path", r.URL.Path)
-	fmt.Println("scheme", r.URL.Scheme)
-	fmt.Fprintf(w, "Hai selezionato:\n")
-	for k, v := range r.Form {
-		fmt.Println("key:", k)
-		fmt.Println("val:", strings.Join(v, ""))
-		fmt.Fprintf(w, "%s:%s \n", k, strings.Join(v, ""))
+	//Initial name contains distro and date
+	name := fmt.Sprintf("arch-%s", time.Now().Format("2006-01-02"))
+
+	// For every value in form we get the data and we put it into a struct, this will
+	// create an univoque name for the ISO
+	if len(r.Form) > 0 {
+		for k, v := range r.Form {
+			value := strings.Join(v, "")
+			fmt.Println("key:", k)
+			fmt.Println("val:", value)
+			name = fmt.Sprintf("%s-%s", string(k[0]), string(value[0]))
+			fmt.Fprintf(w, "%s:%s <br>", k, strings.Join(v, ""))
+		}
+		distros = append(distros, distro{name: name, created: false})
+	} else {
+		fmt.Println("Error parsing form data")
 	}
+	found := checkDistro(name)
+	if !found {
+		fmt.Fprintf(w, "Creating Distro: %s<br>", name)
+	} else {
+		fmt.Fprint(w, "Distro already existing, if there are some problems contact me at deckedspring@gmail.com")
+	}
+}
+
+func checkDistro(name string) bool {
+	found := false
+	for _, v := range distros {
+		vSplitted := strings.Split(v.name, "-")
+		currentSplitted := strings.Split(name, "-")
+		if vSplitted[2] == currentSplitted[2] {
+			found = true
+			break
+		}
+	}
+	return found
 }
 
 //Global array of distros
 var distros []distro
+var response string
 
 func main() {
-
+	response = "<table>"
+	http.HandleFunc("/get_schedule", getSchedule)
 	http.HandleFunc("/distro_creator", createDistro)
 	log.Fatal(http.ListenAndServe(":8080", nil))
+
 }
